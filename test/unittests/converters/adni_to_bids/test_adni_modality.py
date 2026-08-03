00ecb0398c0b93834eaa2740a537accfe0aaa56d
AliceJoubert <158147135+AliceJoubert@users.noreply.github.com>
2026-08-03 14:21:34 +0200

    [REF] ADNI-to-BIDS : add option to convert other processing steps for all PET modalities (#1673)
    * Use tracer for error message
    
    * Remove step from value
    
    * todos
    
    * Move to all utils
    
    * Add unit tests
    
    * Fix import
    
    * Move everything and test
    
    * Add option to client
    
    * Fix import
    
    * Move PPS to avoid circular imports
    
    * Move class for tests
    
    * Newline
    
    * Update doc
    
    * Docstrings
    
    * Enrich ADNIPETPreprocessingStep class
    
    * Rename step 4
    
    * Update utils
    
    * Write functions as properties
    
    * Create new module
    
    * Add modality module
    
    * Extend to all pet modalities
    
    * Doc small changes
    
    * Remove FDG_PET_UNIFORM modality
    
    * Fix tests
    
    * Add default depending on modality
    
    * Modify doc
    
    * Update cli
    
    * Move from function def to body
    
    * Modify error message
    
    * Correct test
    
    * Fix test
    
    * add pps in unit tests
    
    * Explicit usage of json_sidecar modality parameter

---
+++ test/unittests/converters/adni_to_bids/test_adni_modality.py
@@ -0,0 +1,150 @@
+import pytest
+
+from clinica.converters.adni_to_bids._modality import (
+    ADNIModalityConverter,
+    ADNIPETPreprocessingStep,
+)
+
+
+@pytest.mark.parametrize(
+    "modality",
+    ["PET_FDG", "PET_PIB", "PET_AV45", "PET_TAU", "PET_FBB"],
+)
+def test_adni_modality_converter_is_pet_true(modality):
+    assert ADNIModalityConverter[f"{modality}"].is_pet
+
+
+@pytest.mark.parametrize("modality", ["T1", "DWI", "FLAIR", "FMRI", "FMAP"])
+def test_adni_modality_converter_is_pet_false(modality):
+    assert not ADNIModalityConverter[f"{modality}"].is_pet
+
+
+@pytest.mark.parametrize(
+    "modality, expected",
+    [
+        ("T1", "anat"),
+        ("DWI", "dwi"),
+        ("FLAIR", "anat"),
+        ("FMRI", "func"),
+        ("FMAP", "fmap"),
+        ("PET_FDG", "pet"),
+        ("PET_PIB", "pet"),
+        ("PET_AV45", "pet"),
+        ("PET_TAU", "pet"),
+    ],
+)
+def test_get_output_path(modality, expected):
+    assert ADNIModalityConverter[f"{modality}"].output_folder == expected
+
+
+@pytest.mark.parametrize(
+    "modality",
+    ["PET_FDG", "PET_PIB", "PET_AV45", "PET_TAU", "PET_FBB", "T1"],
+)
+def test_write_json_sidecar_false(modality):
+    assert not ADNIModalityConverter[f"{modality}"].json_sidecar
+
+
+@pytest.mark.parametrize("modality", ["DWI", "FMRI", "FMAP", "FLAIR"])
+def test_write_json_sidecar_true(modality):
+    assert ADNIModalityConverter[f"{modality}"].json_sidecar
+
+
+@pytest.mark.parametrize(
+    "modality",
+    [
+        "PET_FDG",
+        "PET_PIB",
+        "PET_AV45",
+        "PET_TAU",
+        "PET_FBB",
+        "T1",
+        "FLAIR",
+    ],
+)
+def test_should_be_centered_true(modality):
+    assert ADNIModalityConverter[f"{modality}"].to_center
+
+
+@pytest.mark.parametrize("modality", ["DWI", "FMRI", "FMAP"])
+def test_should_be_centered_false(modality):
+    assert not ADNIModalityConverter[f"{modality}"].to_center
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        (0, "ADNI Brain PET: Raw"),
+        (1, "Co-registered Dynamic"),
+        (2, "Co-registered, Averaged"),
+        (3, "Coreg, Avg, Standardized Image and Voxel Size"),
+        (4, "Coreg, Avg, Std Img and Vox Siz, Uniform Resolution"),
+        (5, "Coreg, Avg, Std Img and Vox Siz, Uniform 6mm Res"),
+    ],
+)
+def test_adni_preprocessing_step_from_value(value, expected):
+    assert expected == ADNIPETPreprocessingStep.from_step_value(value).value
+
+
+@pytest.mark.parametrize("value", [1.2, "truc"])
+def test_adni_preprocessing_step_from_value_error(value):
+    with pytest.raises(ValueError):
+        ADNIPETPreprocessingStep.from_step_value(value)
+
+
+@pytest.mark.parametrize(
+    "modality, pet_step, expected",
+    [
+        ("T1", None, "_T1w"),
+        ("DWI", None, "_dwi"),
+        ("FLAIR", None, "_FLAIR"),
+        ("FMRI", None, "_task-rest_bold"),
+        ("FMAP", None, "_fmap"),
+        (
+            "PET_FDG",
+            ADNIPETPreprocessingStep.STEP4_6MM,
+            "_trc-18FFDG_rec-coregiso6_pet",
+        ),
+        (
+            "PET_PIB",
+            ADNIPETPreprocessingStep.STEP4_8MM,
+            "_trc-11CPIB_rec-coregiso8_pet",
+        ),
+        ("PET_TAU", ADNIPETPreprocessingStep.STEP0, "_trc-18FAV1451_rec-raw_pet"),
+    ],
+)
+def test_get_output_filename(modality, pet_step, expected):
+    from clinica.converters.adni_to_bids._modality import _get_output_filename
+
+    assert (
+        _get_output_filename(
+            ADNIModalityConverter[f"{modality}"], pet_preprocessing_step=pet_step
+        )
+        == expected
+    )
+
+
+def test_get_output_filename_with_tracer():
+    from clinica.converters.adni_to_bids._modality import _get_output_filename
+    from clinica.utils.pet import Tracer
+
+    assert (
+        _get_output_filename(
+            ADNIModalityConverter.PET_AV45,
+            tracer=Tracer.AV45,
+            pet_preprocessing_step=ADNIPETPreprocessingStep.STEP2,
+        )
+        == "_trc-18FAV45_rec-coregavg_pet"
+    )
+
+
+def test_get_output_filename_with_step():
+    from clinica.converters.adni_to_bids._modality import _get_output_filename
+
+    assert (
+        _get_output_filename(
+            ADNIModalityConverter.PET_AV45,
+            pet_preprocessing_step=ADNIPETPreprocessingStep.STEP4_8MM,
+        )
+        == "_trc-18FAV45_rec-coregiso8_pet"
+    )
